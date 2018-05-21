f9b8ead3e7108617aa07d04ed29955467aa5296c
Puklajs <calvin.azzopardi.b42350@mcast.edu.mt>
2018-05-21 22:46:42 +0200

    Third Commit
    Added Contact us page.
    Implemented mailto function to send feedback on business email and a carbon copy to personal email.

---
+++ Project Assignment/contactUs.php
@@ -0,0 +1,9 @@
+<?php session_start(); ?>
+<?php
+    if (isset($_SESSION['user'])) {
+        include_once("loggedNav.php");
+    }else{
+        include_once("guestNav.php");
+    }
+    include_once("contactUsCode.php");
+?>
